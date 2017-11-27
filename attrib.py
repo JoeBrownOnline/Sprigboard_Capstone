31de4f92f70b57fda5f3c49aedbdd5887fa73350
Joe Brown <joebrownonline@gmail.com>
2017-11-27 15:53:20 -0600

    Author Attribution Script - work in progress

---
+++ attrib.py
@@ -0,0 +1,119 @@
+import numpy as np
+import nltk
+import glob
+import os
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.cluster import KMeans
+from scipy.cluster.vq import whiten
+
+sentence_tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+word_tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+')
+
+# Load data
+data_folder = r"C:/Users/Joe/Desktop/books/"
+files = sorted(glob.glob(os.path.join(data_folder, "*.txt")))
+books = []
+for fn in files:
+    with open(fn, 'r', encoding='utf8') as f:
+        books.append(f.read().lower().replace("\xa0", " ").replace("\n", " ").replace("\ufeff", " "))
+
+all_text = ' '.join(books)
+
+
+def PredictAuthors(fvs):
+    """
+    Use k-means clustering to fit a model
+    """
+    km = KMeans(n_clusters=5, init='k-means++', n_init=10, verbose=0)
+    km.fit(fvs)
+
+    return km
+
+
+def LexicalFeatures():
+    """
+    Compute feature vectors for word and punctuation features
+    """
+    num_chapters = len(books)
+    fvs_lexical = np.zeros((len(books), 3), np.float64)
+    fvs_punct = np.zeros((len(books), 3), np.float64)
+    for e, text in enumerate(books):
+        # note: the nltk.word_tokenize includes punctuation
+        tokens = nltk.word_tokenize(text.lower())
+        words = word_tokenizer.tokenize(text.lower())
+        sentences = sentence_tokenizer.tokenize(text)
+        vocab = set(words)
+        words_per_sentence = np.array([len(word_tokenizer.tokenize(s))
+                                       for s in sentences])
+
+        # average number of words per sentence
+        fvs_lexical[e, 0] = words_per_sentence.mean()
+        # sentence length variation
+        fvs_lexical[e, 1] = words_per_sentence.std()
+        # Lexical diversity
+        fvs_lexical[e, 2] = len(vocab) / float(len(words))
+
+        # Commas per sentence
+        fvs_punct[e, 0] = tokens.count(',') / float(len(sentences))
+        # Semicolons per sentence
+        fvs_punct[e, 1] = tokens.count(';') / float(len(sentences))
+        # Colons per sentence
+        fvs_punct[e, 2] = tokens.count(':') / float(len(sentences))
+
+    # apply whitening to decorrelate the features
+    fvs_lexical = whiten(fvs_lexical)
+    fvs_punct = whiten(fvs_punct)
+
+    return fvs_lexical, fvs_punct
+
+
+def BagOfWords():
+    """
+    Compute the bag of words feature vectors, based on the most common words
+     in the whole book
+    """
+    # get most common words in the whole book
+    all_tokens = nltk.word_tokenize(all_text)
+    fdist = nltk.FreqDist(all_tokens)
+    vocab = list(fdist.keys())
+
+    # use sklearn to create the bag for words feature vector for each chapter
+    vectorizer = CountVectorizer(vocabulary=vocab, tokenizer=nltk.word_tokenize)
+    fvs_bow = vectorizer.fit_transform(books).toarray().astype(np.float64)
+
+    # normalise by dividing each row by its Euclidean norm
+    fvs_bow /= np.c_[np.apply_along_axis(np.linalg.norm, 1, fvs_bow)]
+
+    return fvs_bow
+
+
+def SyntacticFeatures():
+    """
+    Extract feature vector for part of speech frequencies
+    """
+    def token_to_pos(ch):
+        tokens = nltk.word_tokenize(ch)
+        return [p[1] for p in nltk.pos_tag(tokens)]
+
+    chapters_pos = [token_to_pos(ch) for ch in books]
+    pos_list = ['NN', 'NNP', 'DT', 'IN', 'JJ', 'NNS']
+    fvs_syntax = np.array([[ch.count(pos) for pos in pos_list]
+                           for ch in chapters_pos]).astype(np.float64)
+
+    # normalise by dividing each row by number of tokens in the chapter
+    fvs_syntax /= np.c_[np.array([len(ch) for ch in chapters_pos])]
+
+    return fvs_syntax
+
+
+if __name__ == '__main__':
+    feature_sets = list(LexicalFeatures())
+    feature_sets.append(BagOfWords())
+    feature_sets.append(SyntacticFeatures())
+
+    classifications = [PredictAuthors(fvs).labels_ for fvs in feature_sets]
+    for results in classifications:
+        # we know that emma was written by Jane Austen
+        if results[2] == 0:
+            results = 1 - results
+        print(' '.join([str(a) for a in results]))
